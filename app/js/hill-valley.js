2e2306326ade95d26736c38f824f4a80bd1e7360
Michael Tempest <info@michaeltempest.com>
2015-03-17 22:49:40 +0000

    Update hill valley

--- app/js/hill-valley.js
+++ app/js/hill-valley.js
@@ -4,13 +4,25 @@ var s = new Snap('.hill-valley'),
   hillValley,
   initialise,
   setupClouds,
+  animateText,
+  textElems,
+  cloudWidth,
   cloud,
   clouds,
+  cloudsCont,
   tree,
   leaves,
   treeDim = [],
   leavesDim = [],
+  car,
+  animateClouds,
+  animateCar,
+  carStartMatrix = new Snap.Matrix(),
+  carMidMatrix = new Snap.Matrix(),
+  carEndMatrix = new Snap.Matrix(),
   animateTrees,
+  treeAnim,
+  leavesAnim,
   loopAnimation;
 
 
@@ -35,12 +47,24 @@ Snap.load('img/hill-valley.svg', function (response) {
 initialise = function () {
   tree = s.selectAll('.tree');
   leaves = s.selectAll('.leaves');
+  car = s.select('.car');
+  cloudsCont = s.select('.hill-valley__clouds');
+  textElems = s.selectAll('.text');
 
   cloud = s.select('.cloud');
+
   setupClouds();
+
+  carStartMatrix.rotate(10);
+  carStartMatrix.translate(0,-50);
+  carMidMatrix.rotate(-15);
+  carMidMatrix.translate(300,-20);
+  carEndMatrix.rotate(-10);
+  carEndMatrix.translate(-20,10);
 
   for (var i = 0; i < tree.length; i++) {
     treeDim.push(tree[i].getBBox());
+    leavesDim.push(leaves[i].getBBox());
   };
 
   loopAnimation();
@@ -48,13 +72,14 @@ initialise = function () {
 
 setupClouds = function () {
   var containerHeight = s.node.offsetHeight / 4,
-    containerWidth = s.select('#a').getBBox().w,
     numberOfClouds = 10;
+
+  cloudWidth = s.select('#a').getBBox().w;
 
   clouds = s.g();
 
   for (var i = numberOfClouds; i >= 0; i--) {
-    var x = Math.floor(Math.random() * containerWidth),
+    var x = Math.floor(Math.random() * cloudWidth),
         y = -Math.floor(Math.random() * containerHeight),
         newCloud = cloud.use(),
         randomScale = Math.random() * (0.9 - 0.2) + 0.2;
@@ -70,50 +95,116 @@ setupClouds = function () {
   }
 
 
-  s.select('.cloud').before(clouds.attr({
-    class: 'hill-valley__clouds'
-  }));
+  cloudsCont.append(clouds);
 
-  s.select('.cloud').before(clouds.use().attr({
-    x: containerWidth,
-    opacity: 1,
-    class: 'hill-valley__clouds'
-  }));
+  for (var i = 0; i < 5; i++) {
+    cloudsCont.append(clouds.use().attr({
+      x: cloudWidth * (i + 1),
+      opacity: 1
+    }));
+  }
   clouds.add(cloud);
 };
 
+animateText = function () {
+  var amount = 20/textElems.length;
+
+  setTimeout(function () {
+    for (var i = 1; i < textElems.length; i++) {
+      textElems[i].animate({
+        'transform': 't' + (amount * i) + ',-' + (amount * i)
+      }, 250, mina.easeinout, function () {
+        setTimeout(function () {
+          textElems.animate({
+            transform: 't0,0'
+          }, 750, mina.easeinout);
+        }, 500);
+      });
+    };
+  }, 1250);
+};
+
 loopAnimation = function () {
-  console.log('Loop');
   animateTrees();
+  animateCar();
+  animateClouds();
+  animateText();
 
   setTimeout(loopAnimation, 4000);
 };
 
-animateTrees = function () {
-
-  // leaves.transform('r0,0,0');
-
-  // leaves.animate({
-  //   transform: 'r-10,' + (leaves.getBBox().x + (leaves.getBBox().width / 2)) + ',' + (leaves.getBBox().y + leaves.getBBox().height)
-  // }, 2000, function () {
-  //   leaves.animate({
-  //     transform: 'r25,' + (leaves.getBBox().x + (leaves.getBBox().width / 2)) + ',' + (leaves.getBBox().y + leaves.getBBox().height)
-  //   }, 20, mina.easeinout, function (){
-  //     leaves.animate({
-  //       transform: 'r0,' + (leaves.getBBox().x + (leaves.getBBox().width / 2)) + ',' + (leaves.getBBox().y + leaves.getBBox().height)
-  //     }, 1000, mina.elastic);
-  //   });
-  // });
-
-  for (var i = tree.length - 1; i >= 0; i--) {
-    tree[i].animate({
-      transform: 'r-5,' + (treeDim[i].x + (treeDim[i].width / 2)) + ',' + (treeDim[i].y + treeDim[i].height)
-    }, 2000, function (obj, b) {
-      console.log(this);
-      s.select('#' + this.id).animate({
-        transform: 'r0,0,0'
-      }, 1000, mina.elastic);
+animateCar = function () {
+  car.animate({
+    transform: carStartMatrix
+  }, 1250, mina.easeinout, function () {
+    car.animate({
+      transform: carMidMatrix
+    }, 250, function () {
+      car.transform('t-300,-150');
+      setTimeout(function () {
+        car.animate({
+          transform: carEndMatrix
+        }, 750, mina.backout, function () {
+          car.animate({
+            transform: 't0,0'
+          }, 750)
+        });
+      }, 500);
     });
+  });
+};
+
+animateClouds = function () {
+  cloudsCont.transform('t0,0');
+  cloudsCont.animate({
+    transform: 't-25,0'
+  }, 1250, function () {
+    cloudsCont.animate({
+      transform: 't-' +  ((cloudWidth * 5) - 50) + ',0'
+    }, 750, mina.easein, function () {
+      cloudsCont.animate({
+        transform: 't-' +  (cloudWidth * 5) + ',0'
+      }, 1750);
+    });
+  });
+};
+
+animateTrees = function () {
+  for (var i = tree.length - 1; i >= 0; i--) {
+    treeAnim(i);
+    leavesAnim(i);
   };
 };
 
+leavesAnim = function (index) {
+  leaves[index].animate({
+    transform: 'r-10,' + (leavesDim[index].x + (leavesDim[index].width / 2)) + ',' + (leavesDim[index].y + leavesDim[index].height)
+  }, 1250, function () {
+    leaves[index].animate({
+      transform: 'r25,' + (leavesDim[index].x + (leavesDim[index].width / 2)) + ',' + (leavesDim[index].y + leavesDim[index].height)
+    }, 20, mina.easeinout, function (){
+      leaves[index].animate({
+        transform: 'r0,' + (leavesDim[index].x + (leavesDim[index].width / 2)) + ',' + (leavesDim[index].y + leavesDim[index].height)
+      }, 1000, mina.elastic);
+    });
+  });
+
+};
+
+treeAnim = function (index) {
+  tree[index].animate({
+    transform: 'r-5,' + (treeDim[index].x + (treeDim[index].width / 2)) + ',' + (treeDim[index].y + treeDim[index].height)
+  }, 1250, function () {
+    tree[index].animate({
+      transform: 'r0,0,0'
+    }, 1000, mina.elastic, function () {
+      tree[index].animate({
+        transform: 'r8,' + (treeDim[index].x + (treeDim[index].width / 2)) + ',' + (treeDim[index].y + treeDim[index].height)
+      }, 150, function () {
+        tree[index].animate({
+          transform: 'r0,' + (treeDim[index].x + (treeDim[index].width / 2)) + ',' + (treeDim[index].y + treeDim[index].height)
+        }, 1000, mina.elastic);
+      });
+    });
+  });
+};
